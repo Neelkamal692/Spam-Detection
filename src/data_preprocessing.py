de22b916eb29a19896bd748ade63cd641d60b94c
neelsahu <neelkamal.sahu@transmissionagency.com>
2026-02-22 20:32:33 +0530

    pipeline code ready

---
+++ src/data_preprocessing.py
@@ -0,0 +1,92 @@
+import os
+import logging_config
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import string
+import nltk
+
+logger = logging_config.setup_logging('DataPreProcessing')
+
+def transform_text(text:str) -> str:
+    """
+    Transforms the input text by converting it to lowercase, tokenizing, removing stopwords and punctuation, and stemming.
+    """
+    try : 
+        ps = PorterStemmer()
+        # Convert to lowercase
+        text = text.lower()
+        # Tokenize the text
+        text = nltk.word_tokenize(text)
+        # Remove non-alphanumeric tokens
+        text = [word for word in text if word.isalnum()]
+        # Remove stopwords and punctuation
+        text = [word for word in text if word not in stopwords.words('english') and word not in string.punctuation]
+        # Stem the words
+        text = [ps.stem(word) for word in text]
+        # Join the tokens back into a single string
+        return " ".join(text)
+    except Exception as e :
+        logger.error("Some Unexpected error occured during transformation %s", e)
+        raise
+
+def preprocess_df(df:pd.DataFrame, text_column='text', target_column='target') -> pd.DataFrame:
+    """
+    Preprocesses the DataFrame by encoding the target column, removing duplicates, and transforming the text column.
+    """
+    try:
+        logger.debug('Starting preprocessing for DataFrame')
+        # Encode the target column
+        encoder = LabelEncoder()
+        df[target_column] = encoder.fit_transform(df[target_column])
+        logger.debug('Target column encoded')
+
+        # Remove duplicate rows
+        df = df.drop_duplicates(keep='first')
+        logger.debug('Duplicates removed....')
+        
+        # Apply text transformation to the specified text column
+        df.loc[:, text_column] = df[text_column].apply(transform_text)
+        logger.debug('Text column transformed')
+        return df
+    
+    except KeyError as e:
+        logger.error('Column not found: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Error during text normalization: %s', e)
+        raise
+
+def main(text_column='text', target_column='target'):
+    """
+    Main function to load raw data, preprocess it, and save the processed data.
+    """
+    try:
+        # Fetch the data from data/raw
+        train_data = pd.read_csv('./data/raw/train.csv')
+        test_data = pd.read_csv('./data/raw/test.csv')
+        logger.debug('Data loaded properly')
+
+        # Transform the data
+        train_processed_data = preprocess_df(train_data, text_column, target_column)
+        test_processed_data = preprocess_df(test_data, text_column, target_column)
+
+        # Store the data inside data/processed
+        data_path = os.path.join("./data", "interim")
+        os.makedirs(data_path, exist_ok=True)
+        
+        train_processed_data.to_csv(os.path.join(data_path, "train_processed.csv"), index=False)
+        test_processed_data.to_csv(os.path.join(data_path, "test_processed.csv"), index=False)
+        
+        logger.debug('Processed data saved to %s', data_path)
+    except FileNotFoundError as e:
+        logger.error('File not found: %s', e)
+    except pd.errors.EmptyDataError as e:
+        logger.error('No data: %s', e)
+    except Exception as e:
+        logger.error('Failed to complete the data transformation process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
